408ded943378759e6e80d50436dac1a2fd8b9619
tania <taniauniaokoth@gmail.com>
2026-05-06 18:33:15 +0300

    Enhance header styling with sticky position and backdrop blur for improved visibility and aesthetics

--- script.js
+++ script.js
@@ -90,3 +90,10 @@ if (moodSelect && preview) {
     preview.textContent = "You're feeling: " + moodSelect.value;
   });
 }
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
